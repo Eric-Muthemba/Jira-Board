e73c6818624c0e3d72307410c804f17b29699761
Eric Muthemba Kiarie <emkiarie0@gmail.com>
2024-01-20 17:11:37 +0100

    Test fix

--- test/models/card_member_test.exs
+++ test/models/card_member_test.exs
@@ -3,7 +3,7 @@ defmodule PhoenixJiraBoard.CardMemberTest do
 
   alias PhoenixJiraBoard.CardMember
 
-  @valid_attrs %{}
+  @valid_attrs %{card_id: 1, user_board_id: 1}
   @invalid_attrs %{}
 
   test "changeset with valid attributes" do
