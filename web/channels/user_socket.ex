17e06518100215d7c5d2bca080d15eb9c97f1d75
Eric Muthemba Kiarie <emkiarie0@gmail.com>
2024-12-26 17:50:20 +0100

    User socket and board channel

---
+++ web/channels/user_socket.ex
@@ -0,0 +1,29 @@
+defmodule PhoenixJiraBoard.UserSocket do
+  use Phoenix.Socket
+
+  alias PhoenixJiraBoard.Repo
+  alias PhoenixJiraBoard.User
+
+
+  # Channels
+  channel "boards:*", PhoenixJiraBoard.BoardChannel
+
+  # Transports
+  transport :websocket, Phoenix.Transports.WebSocket
+  transport :longpoll, Phoenix.Transports.LongPoll
+
+  def connect(%{"token" => token}, socket) do
+    case Guardian.decode_and_verify(token) do
+      {:ok, claims} ->
+        user_id = claims["sub"] |> String.replace("User:", "") |> String.to_integer
+        user = Repo.get!(User, user_id)
+
+        {:ok, assign(socket, :current_user, user)}
+      {:error, _reason} ->
+        :error
+    end
+  end
+  def connect(_params, _socket), do: :error
+
+  def id(socket), do: "users_socket:#{socket.assigns.current_user.id}"
+end
